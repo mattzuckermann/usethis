616ccba61f0d181e9dea7277d50e3baa2f087408
Matt Zuckermann <matt@mattzuckermann.dev>
2020-11-29 09:12:06 -0800

    added pages and finished nav bar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,47 +1,49 @@
 import Head from "next/head";
 import { withApollo } from "../lib/apollo";
 import { FC } from "react";
+import Header from "../components/Header/Header";
 import SplashAnimation from "../components/SplashAnimation";
-import UserForm from "../components/UserForm";
-import ResultForm from "../components/ResultForm";
-import UserList from "../components/UserList";
-import ResultList from "../components/ResultList";
+import FormSection from "../components/Forms/FormSection";
 import Footer from "../components/Footer";
+
+const linkArray = [
+  { name: "API", path: "/api/graphql", targetBlank: true },
+  { name: "Login", path: "/login", targetBlank: false },
+  { name: "Study", path: "/study", targetBlank: false },
+  { name: "Test", path: "/test", targetBlank: false },
+];
 
 const Home: FC = () => {
   return (
     <div>
       <Head>
         <link rel="icon" href="/favicon.svg" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inconsolata:wght@300&display=swap"
+          rel="stylesheet"
+        />
       </Head>
+      <Header
+        brand="Let's Learn Together"
+        links={linkArray}
+        absolute={true}
+        color="primary"
+      />
       <SplashAnimation />
-
-      <div className="container">
-        <UserForm />
-        <UserList />
-        <ResultForm />
-        <ResultList />
-        <Footer />
-      </div>
+      <FormSection />
+      <Footer />
       <style jsx global>{`
         html,
         body {
+          background-image: url("/Homepage.webp");
+          background-color: #524762;
+          background-repeat: "no-repeat";
           scroll-behavior: smooth;
-          background-position: center;
-          background-size: 50px;
-          background-repeat: no-repeat;
           padding: 0;
           margin: 0;
           font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
             Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
             sans-serif;
-        }
-        .container {
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
         }
         * {
           box-sizing: border-box;
---
+++ pages/test/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const index = () => {
+  return <div>Let's test!</div>;
+};
+
+export default index;
