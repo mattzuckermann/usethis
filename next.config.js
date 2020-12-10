7b9d85c7bee34d8434f0449f34d244186a4c36d1
Matt Zuckermann <matt@mattzuckermann.dev>
2020-12-10 05:09:31 -0800

    build: edited NextJS, TypeScript, Git, and NPM configs

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 module.exports = {
   webpack: (config) => {
     config.module.rules.push({
@@ -9,5 +11,17 @@ module.exports = {
   },
   webpackDevMiddleware: (config) => {
     return config;
+  },
+  async redirects() {
+    return [
+      {
+        source: "/login",
+        destination: "/api/auth/signin",
+        permanent: true,
+      },
+    ];
+  },
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
   },
 };
