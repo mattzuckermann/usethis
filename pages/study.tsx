466645c622149216d537e7a860bb9fe354c62424
Matt Zuckermann <matt@mattzuckermann.dev>
2021-01-06 15:45:13 -0800

    refactor: added content to study section and made images mobile responsive

--- pages/study.tsx
+++ pages/study.tsx
@@ -7,6 +7,36 @@ const Study = ({ user }: { user: User }): ReactElement => {
   return (
     <main className="layout">
       <h1>{user?.name ? `Let's study, ${user.name}!` : `Let's study!`}</h1>
+      <section>
+        <div className="flexCentered card">
+          <h2>w3schools</h2>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://www.w3schools.com/js/js_this.asp"
+          >
+            <img
+              width="300"
+              alt="w3schools-logo"
+              src="https://imstartpage.com/wp-content/uploads/2015/01/W3Schools-logo.png"
+            />
+          </a>
+        </div>
+        <div className="flexCentered card">
+          <h2>jstutorial</h2>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://www.javascripttutorial.net/javascript-this/"
+          >
+            <img
+              width="300"
+              alt="javascript-logo"
+              src="https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png"
+            />
+          </a>
+        </div>
+      </section>
     </main>
   );
 };
